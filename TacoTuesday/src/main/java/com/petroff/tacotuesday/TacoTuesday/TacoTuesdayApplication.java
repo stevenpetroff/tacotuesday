6177ee08c023fcae740fd6f2dd2336302821aaf8
Steven <Stevenpetroff@gmail.com>
2017-11-05 19:40:29 +0000

    Init

---
+++ TacoTuesday/src/main/java/com/petroff/tacotuesday/TacoTuesday/TacoTuesdayApplication.java
@@ -0,0 +1,12 @@
+package com.petroff.tacotuesday.TacoTuesday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoTuesdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoTuesdayApplication.class, args);
+	}
+}
---
+++ TacoTuesday/src/test/java/com/petroff/tacotuesday/TacoTuesday/TacoTuesdayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.petroff.tacotuesday.TacoTuesday;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoTuesdayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
